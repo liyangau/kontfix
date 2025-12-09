0d22e1dfe00c5aca6d5a44a55fe29cb6f5c832b1
liyangau <d@aufomm.com>
2025-12-09 11:56:57 +1100

    refactor local storage to simplify its uasge

--- storage/local.nix
+++ storage/local.nix
@@ -45,6 +45,18 @@ in
               }
             ];
           };
+
+      create_clusters_dir =
+        mkIf ((localStoragePinnedCertControlPlanes != { }) || (localStoragePkiCertControlPlanes != { }) || (localStorageClusterConfigOnlyControlPlanes != { }))
+          {
+            provisioner = [
+              {
+                local-exec = {
+                  command = "mkdir -p \${path.module}/clusters && chmod 700 \${path.module}/clusters";
+                };
+              }
+            ];
+          };
     };
 
     resource.local_file = mkMerge [
@@ -53,9 +65,9 @@ in
         name: cp:
         nameValuePair "${name}_pinned_cert" {
           content = "\${tls_self_signed_cert.${name}.cert_pem}";
-          filename = "\${path.module}/certs/${name}-cert.pem";
-          file_permission = "0400";
-          directory_permission = "0700";
+          filename = "\${path.module}/certs/${name}/cert.pem";
+          file_permission = "0444";
+          directory_permission = "0755";
           depends_on = [ "null_resource.create_cert_dir" ];
           lifecycle = [
             {
@@ -71,9 +83,9 @@ in
         name: cp:
         nameValuePair "${name}_pinned_key" {
           content = "\${tls_private_key.${name}.private_key_pem}";
-          filename = "\${path.module}/certs/${name}-key.pem";
-          file_permission = "0400";
-          directory_permission = "0700";
+          filename = "\${path.module}/certs/${name}/key.pem";
+          file_permission = "0444";
+          directory_permission = "0755";
           depends_on = [ "null_resource.create_cert_dir" ];
         }
       ) localStoragePinnedCertControlPlanes)
@@ -83,9 +95,9 @@ in
         name: cp:
         nameValuePair "${name}_pki_cert" {
           content = "\${vault_pki_secret_backend_cert.${name}.certificate}";
-          filename = "\${path.module}/certs/${name}-pki-cert.pem";
-          file_permission = "0400";
-          directory_permission = "0700";
+          filename = "\${path.module}/certs/${name}/cert.pem";
+          file_permission = "0444";
+          directory_permission = "0755";
           depends_on = [ "null_resource.create_cert_dir" ];
         }
       ) localStoragePkiCertControlPlanes)
@@ -94,9 +106,9 @@ in
         name: cp:
         nameValuePair "${name}_pki_key" {
           content = "\${vault_pki_secret_backend_cert.${name}.private_key}";
-          filename = "\${path.module}/certs/${name}-pki-key.pem";
-          file_permission = "0400";
-          directory_permission = "0700";
+          filename = "\${path.module}/certs/${name}/key.pem";
+          file_permission = "0444";
+          directory_permission = "0755";
           depends_on = [ "null_resource.create_cert_dir" ];
         }
       ) localStoragePkiCertControlPlanes)
@@ -107,12 +119,13 @@ in
         nameValuePair "${name}_token" {
           content = "\${jsonencode({
             token = konnect_system_account_access_token.${name}.token
+            api_addr = \"https://${cp.region}.api.konghq.com\"
             expires_at = konnect_system_account_access_token.${name}.expires_at
             created_at = konnect_system_account_access_token.${name}.created_at
           })}";
           filename = "\${path.module}/tokens/${cp.region}_cp_${cp.originalName}.json";
-          file_permission = "0400";
-          directory_permission = "0700";
+          file_permission = "0444";
+          directory_permission = "0755";
           depends_on = [ "null_resource.create_token_dir" ];
         }
       ) localStorageSysAccountControlPlanes)
@@ -124,13 +137,14 @@ in
           nameValuePair "${group.groupName}_group_token" {
             content = "\${jsonencode({
             token = konnect_system_account_access_token.${group.groupName}.token
+            api_addr = \"https://${group.regionName}.api.konghq.com\"
             expires_at = konnect_system_account_access_token.${group.groupName}.expires_at
             created_at = konnect_system_account_access_token.${group.groupName}.created_at
             members = ${builtins.toJSON group.groupConfig.members}
           })}";
             filename = "\${path.module}/tokens/${group.regionName}_group_${group.originalName}.json";
-            file_permission = "0400";
-            directory_permission = "0700";
+            file_permission = "0444";
+            directory_permission = "0755";
             depends_on = [ "null_resource.create_token_dir" ];
           }
         )
@@ -144,28 +158,25 @@ in
         )
       )
 
-      # Cluster configuration files
+      # Cluster configuration files (consolidated key-value format without certificates)
       (mapAttrs' (
         name: cp:
-        nameValuePair "${name}_pinned_config" {
-          content = "\${jsonencode({
-          certificate = tls_self_signed_cert.${name}.cert_pem
-          private_key = tls_private_key.${name}.private_key_pem
-          issuing_ca = tls_self_signed_cert.${name}.cert_pem
-          cluster_url = konnect_gateway_control_plane.${name}.config.control_plane_endpoint
-          telemetry_url = konnect_gateway_control_plane.${name}.config.telemetry_endpoint
-          cp_id = konnect_gateway_control_plane.${name}.id
-          cluster_prefix = regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]
-          cluster_control_plane = \"\${regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]}.${cp.region}.cp.konghq.com:443\"
-          cluster_server_name = \"\${regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]}.${cp.region}.cp.konghq.com\"
-          cluster_telemetry_endpoint = \"\${regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]}.${cp.region}.tp.konghq.com:443\"
-          cluster_telemetry_server_name = \"\${regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]}.${cp.region}.tp.konghq.com\"
-          })}";
-          filename = "\${path.module}/certs/${name}-cluster-config.json";
-          file_permission = "0400";
-          directory_permission = "0700";
-          depends_on = [ "null_resource.create_cert_dir" ];
-          lifecycle = [
+        nameValuePair "${name}_cluster_config" {
+          content = ''
+            CLUSTER_URL=''${konnect_gateway_control_plane.${name}.config.control_plane_endpoint}
+            TELEMETRY_URL=''${konnect_gateway_control_plane.${name}.config.telemetry_endpoint}
+            CP_ID=''${konnect_gateway_control_plane.${name}.id}
+            CP_CLUSTER_PREFIX=''${regex("^https://([^/.]+)", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]}
+            CLUSTER_SERVER_NAME=''${replace(konnect_gateway_control_plane.${name}.config.control_plane_endpoint, "https://", "")}
+            CLUSTER_TELEMETRY_SERVER_NAME=''${replace(konnect_gateway_control_plane.${name}.config.telemetry_endpoint, "https://", "")}
+            CP_REGION=${cp.region}
+            CP_NAME=${cp.originalName}
+          '';
+          filename = "\${path.module}/clusters/${name}";
+          file_permission = "0444";
+          directory_permission = "0755";
+          depends_on = [ "null_resource.create_clusters_dir" ];
+          lifecycle = mkIf (localStoragePinnedCertControlPlanes ? name) [
             {
               replace_triggered_by = [
                 "time_rotating.${name}_cert"
@@ -173,51 +184,7 @@ in
             }
           ];
         }
-      ) localStoragePinnedCertControlPlanes)
-
-      (mapAttrs' (
-        name: cp:
-        nameValuePair "${name}_pki_config" {
-          content = "\${jsonencode({
-          certificate = \"\${vault_pki_secret_backend_cert.${name}.certificate}\\n\${vault_pki_secret_backend_cert.${name}.issuing_ca}\"
-          private_key = vault_pki_secret_backend_cert.${name}.private_key
-          issuing_ca = vault_pki_secret_backend_cert.${name}.issuing_ca
-          cluster_url = konnect_gateway_control_plane.${name}.config.control_plane_endpoint
-          telemetry_url = konnect_gateway_control_plane.${name}.config.telemetry_endpoint
-          cp_id = konnect_gateway_control_plane.${name}.id
-          cluster_prefix = regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]
-          cluster_control_plane = \"\${regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]}.${cp.region}.cp.konghq.com:443\"
-          cluster_server_name = \"\${regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]}.${cp.region}.cp.konghq.com\"
-          cluster_telemetry_endpoint = \"\${regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]}.${cp.region}.tp.konghq.com:443\"
-          cluster_telemetry_server_name = \"\${regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]}.${cp.region}.tp.konghq.com\"
-          })}";
-          filename = "\${path.module}/certs/${name}-pki-cluster-config.json";
-          file_permission = "0400";
-          directory_permission = "0700";
-          depends_on = [ "null_resource.create_cert_dir" ];
-        }
-      ) localStoragePkiCertControlPlanes)
-
-      # Cluster-config only local file resources (no certificates)
-      (mapAttrs' (
-        name: cp:
-        nameValuePair "${name}_cluster_config_only" {
-          content = "\${jsonencode({
-          cluster_url = konnect_gateway_control_plane.${name}.config.control_plane_endpoint
-          telemetry_url = konnect_gateway_control_plane.${name}.config.telemetry_endpoint
-          cp_id = konnect_gateway_control_plane.${name}.id
-          cluster_prefix = regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]
-          cluster_control_plane = \"\${regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]}.${cp.region}.cp.konghq.com:443\"
-          cluster_server_name = \"\${regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]}.${cp.region}.cp.konghq.com\"
-          cluster_telemetry_endpoint = \"\${regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]}.${cp.region}.tp.konghq.com:443\"
-          cluster_telemetry_server_name = \"\${regex(\"^https://([^.]+)\\\\.\", konnect_gateway_control_plane.${name}.config.control_plane_endpoint)[0]}.${cp.region}.tp.konghq.com\"
-          })}";
-          filename = "\${path.module}/certs/${name}-cluster-config-only.json";
-          file_permission = "0400";
-          directory_permission = "0700";
-          depends_on = [ "null_resource.create_cert_dir" ];
-        }
-      ) localStorageClusterConfigOnlyControlPlanes)
+      ) (localStoragePinnedCertControlPlanes // localStoragePkiCertControlPlanes // localStorageClusterConfigOnlyControlPlanes))
     ];
   };
 }
